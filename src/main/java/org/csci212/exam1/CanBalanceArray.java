6d07d40db5f481b282b2360fcaae264431fcb8c8
Ramil Lim <ramil.lim@gmail.com>
2026-03-01 09:12:56 -0500

    Add canBalance array problem.

---
+++ src/main/java/org/csci212/exam1/CanBalanceArray.java
@@ -0,0 +1,31 @@
+package org.csci212.exam1;
+
+/**
+ * This is a 1-dimensional array practice problem for Learning Objective ARR.
+ * This is from CodingBat Array-3 -> <a href="https://codingbat.com/prob/p158767">canBalance</a>
+ */
+public class CanBalanceArray {
+    public static void main(String[] args) {
+        // Test cases that return true
+        System.out.println(canBalance(new int[] {1, 1, 1, 2, 1} ));
+        System.out.println(canBalance(new int[] {10, 10} ));
+        System.out.println(canBalance(new int[] {10, 0, 1, -1, 10} ));
+
+        // Test cases that return false
+        System.out.println(canBalance(new int[] {2, 1, 1, 2, 1} ));
+        System.out.println(canBalance(new int[] {2, 3, 4, 1, 2} ));
+    }
+
+    /**
+     * Given a non-empty array, return true if there is a place to split the array so that the sum
+     * of the numbers on one side is equal to the sum of the numbers on the other side.
+     *
+     * @param nums An array of integers that can be positive, negative, or zero.
+     *
+     * @return true if there is a place to split the array so that the sum of the numbers on one
+     * side is equal to the sum of the numbers on the other side, otherwise return false.
+     */
+    public static boolean canBalance(int[] nums) {
+        throw new UnsupportedOperationException("Delete this line and write your solution here.");
+    }
+}
