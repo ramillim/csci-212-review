830adccc6614dc5dbbb84c8279d028088160345e
Ramil Lim <ramil.lim@gmail.com>
2026-03-01 09:36:47 -0500

    Add solution to CanBalanceArray.

--- src/main/java/org/csci212/exam1/CanBalanceArray.java
+++ src/main/java/org/csci212/exam1/CanBalanceArray.java
@@ -10,9 +10,14 @@ public class CanBalanceArray {
         System.out.println(canBalance(new int[] {1, 1, 1, 2, 1} ));
         System.out.println(canBalance(new int[] {10, 10} ));
         System.out.println(canBalance(new int[] {10, 0, 1, -1, 10} ));
+        System.out.println(canBalance(new int[] {1, 2, 3, 1, 0, 2, 3} ));
+
 
         // Test cases that return false
+        System.out.println(canBalance(new int[] {1} ));
+        System.out.println(canBalance(new int[] {1, 2, 3, 1, 0, 1, 3} ));
         System.out.println(canBalance(new int[] {2, 1, 1, 2, 1} ));
+        System.out.println(canBalance(new int[] {2, 1, 1, 1, 4} ));
         System.out.println(canBalance(new int[] {2, 3, 4, 1, 2} ));
     }
 
@@ -26,6 +31,30 @@ public class CanBalanceArray {
      * side is equal to the sum of the numbers on the other side, otherwise return false.
      */
     public static boolean canBalance(int[] nums) {
-        throw new UnsupportedOperationException("Delete this line and write your solution here.");
+        // Algorithm explanation:
+        // This solution uses nested loops. The outer loop iterates through each element in the
+        // array and sums the elements to the left of the split point (exclusive) and then sums
+        // the elements to the right of the split point (inclusive).
+        // If the sums are equal, the method returns true. Otherwise, it performs another iteration
+        // of the outer loop until true is returned or all elements have been iterated through.
+        // If the outer loop is exited with no split point is found, the method returns false.
+
+        for (int splitIndex = 0; splitIndex < nums.length; splitIndex++) {
+            int leftSum = 0;
+            int rightSum = 0;
+
+            // Sums of integers to the left of the split point (exclusive).
+            for (int j = 0; j < splitIndex; j++)
+                leftSum += nums[j];
+
+            // Sums of integers to the right of the split point (inclusive).
+            for (int k = splitIndex; k < nums.length; k++)
+                rightSum += nums[k];
+
+            if (leftSum == rightSum)
+                return true;
+        }
+
+        return false;
     }
 }
