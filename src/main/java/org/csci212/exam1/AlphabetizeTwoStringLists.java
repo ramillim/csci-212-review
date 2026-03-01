b986b7b3353629b73dff2f2f129586858ad8fd50
Ramil Lim <ramil.lim@gmail.com>
2026-03-01 14:30:11 -0500

    Add the AlphebetizeTwoStrings problem.

---
+++ src/main/java/org/csci212/exam1/AlphabetizeTwoStringLists.java
@@ -0,0 +1,45 @@
+package org.csci212.exam1;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ * This practice problem exercises the ARR, LIST1, and STR learning objectives.
+ *
+ * Recall that methods that have been taught in class up to this point include the following. The
+ * duplicate methods are for overloaded methods (same name, different parameters).
+ *
+ * String: charAt, equals, indexOf, length, substring, substring
+ * ArrayList: add, add, get, indexOf, remove, set, size
+ */
+public class AlphabetizeTwoStringLists {
+    public static void main(String[] args) {
+        // Test Case
+        ArrayList<String> list1 = new ArrayList<>(Arrays.asList(
+                "Elizabeta", "Khasan", "Jiang", "Zorica"
+        ));
+        ArrayList<String> list2 = new ArrayList<>(Arrays.asList(
+                "Stanislav", "Humberto", "Megan", "Dhaval", "Eleanore"
+        ));
+
+        String[] result = alphabetize(list1, list2);
+
+        System.out.println(Arrays.toString(result));
+        System.out.println(Arrays.equals(result, new String[] {"Dhaval", "Eleanore", "Elizabeta",
+                "Humberto", "Jiang", "Khasan", "Megan", "Stanislav", "Zorica"}));
+        // The result should print:
+        // ["Dhaval", "Eleanore", "Elizabeta", "Humberto", "Jiang", "Khasan", "Megan", "Stanislav", "Zorica"]
+        // true
+    }
+
+    /**
+     * Alphabetizes two lists of strings. The lists can be of different lengths.
+     *
+     * @param list1 A non-empty list of strings with names consisting entirely of letters.
+     * @param list2 A non-empty list of strings with names consisting entirely of letters.
+     * @return An array containing the names in alphabetical order from both lists.
+     */
+    public static String[] alphabetize(ArrayList<String> list1, ArrayList<String> list2) {
+        throw new UnsupportedOperationException("Delete this line and write your solution here.");
+    }
+}
